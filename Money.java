3be91de2288d3a83be4b467e48507ed9b4876d23
Margaret Kriz <71287192+mrkriz101@users.noreply.github.com>
2023-12-04 17:41:41 -0500

    started expenses

--- Money.java
+++ Money.java
@@ -1,23 +1,27 @@
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-public class Money {
-    public static void main(String[] args) {
-        String userName = "Nadine";
+public class Money extends JFrame {
+    private JComboBox<String> categoryComboBox;
+    private JTextField amountField;
+    private JTextField dateField;
+    private JTextArea descriptionArea;
+    private JTextArea contentArea;
 
-        // Create a JFrame
-        JFrame frame = new JFrame("Money Application");
+    public Money() {
+        setTitle("Money Application");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(720, 480);
+        setLocationRelativeTo(null);
 
         // Create a panel for content
         JPanel contentPanel = new JPanel();
-        JTextArea contentArea = new JTextArea(100, 60);
+        contentArea = new JTextArea(100, 60);
         contentArea.setEditable(false); // Make the content area read-only
-        contentArea.setText("Welcome "+userName+"!");
+        contentArea.setText("Welcome Nadine!");
         contentPanel.add(contentArea);
 
         // Create buttons for navigation
@@ -27,13 +31,14 @@ public class Money {
         JButton homeButton = new JButton("Home");
         JButton exitButton = new JButton("Exit");
 
-        // Add ActionListener to the budget button
+        // Add ActionListener to the home button
         homeButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                contentArea.setText("Welcome "+userName+"!");
+                contentArea.setText("Welcome Nadine!");
             }
         });
+
         // Add ActionListener to the budget button
         budgetButton.addActionListener(new ActionListener() {
             @Override
@@ -46,7 +51,42 @@ public class Money {
         expenseButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                contentArea.setText("Expense logging!");
+                setTitle("Expense Tracker");
+                getContentPane().removeAll();
+                getContentPane().revalidate();
+                getContentPane().repaint();
+
+                JPanel mainPanel = new JPanel();
+                mainPanel.setLayout(new GridLayout(5, 2, 5, 5));
+
+                JLabel categoryLabel = new JLabel("Category:");
+                categoryComboBox = new JComboBox<>(new String[]{"Food", "Home", "Clothes", "Other"});
+                mainPanel.add(categoryLabel);
+                mainPanel.add(categoryComboBox);
+
+                JLabel amountLabel = new JLabel("Amount:");
+                amountField = new JTextField(10);
+                mainPanel.add(amountLabel);
+                mainPanel.add(amountField);
+
+                JLabel dateLabel = new JLabel("Date (dd/MM/yyyy):");
+                dateField = new JTextField(10);
+                mainPanel.add(dateLabel);
+                mainPanel.add(dateField);
+
+                JLabel descriptionLabel = new JLabel("Description:");
+                descriptionArea = new JTextArea(4, 20);
+                descriptionArea.setLineWrap(true);
+                JScrollPane scrollPane = new JScrollPane(descriptionArea);
+                mainPanel.add(descriptionLabel);
+                mainPanel.add(scrollPane);
+
+                JButton submitButton = new JButton("Submit");
+                submitButton.addActionListener(new SubmitButtonListener());
+                mainPanel.add(submitButton);
+
+                add(mainPanel);
+                setVisible(true);
             }
         });
 
@@ -75,12 +115,53 @@ public class Money {
         navPanel.add(exitButton);
 
         // Add panels to the frame
-        frame.add(contentPanel, BorderLayout.CENTER);
-        frame.add(navPanel, BorderLayout.SOUTH);
+        add(contentPanel, BorderLayout.CENTER);
+        add(navPanel, BorderLayout.SOUTH);
 
-        // Set frame properties
-        frame.setSize(720, 480);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
+        setVisible(true);
+    }
+
+    private class SubmitButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String category = (String) categoryComboBox.getSelectedItem();
+            String amountText = amountField.getText();
+            String dateText = dateField.getText();
+            String description = descriptionArea.getText();
+
+            // Validate inputs (you can add more validation as needed)
+            if (amountText.isEmpty() || dateText.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "Please enter amount and date.");
+                return;
+            }
+
+            double amount = Double.parseDouble(amountText);
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+            Date date;
+            try {
+                date = dateFormat.parse(dateText);
+            } catch (java.text.ParseException ex) {
+                JOptionPane.showMessageDialog(null, "Invalid date format. Please use dd/MM/yyyy.");
+                return;
+            }
+
+            // Create an object of Expenses
+           // Expenses expense = new Expenses((int) amount, category, dateText, description);
+
+            // Process the expense data (you can add your own logic here)
+            // For now, just display the entered data
+           // String expenseInfo = "Category: " + expense.getCategory() + "\nAmount: $" + expense.getAmount() +
+           //         "\nDate: " + expense.getDate() + "\nDescription: " + expense.getDescription();
+           // JOptionPane.showMessageDialog(null, "Expense Recorded:\n" + expenseInfo);
+
+            // Clear input fields after submission
+            amountField.setText("");
+            dateField.setText("");
+            descriptionArea.setText("");
+        }
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new Money());
     }
 }
