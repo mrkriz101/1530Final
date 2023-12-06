0c1b72ed9a547804ffc92cb0c11977b623297c42
Margaret Kriz <71287192+mrkriz101@users.noreply.github.com>
2023-12-06 15:50:09 -0500

    test file added

---
+++ MoneyTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+
+import java.awt.event.ActionEvent;
+
+public class MoneyTest {
+
+    @Test
+    public void testSavingsButtonPressed() {
+        Money money = new Money(); // Instantiate your Money class
+        money.frame = Mockito.mock(JFrame.class); // Mock the JFrame
+
+        // Simulate a button click on the savings goals button
+        money.savingsButton.getActionListeners()[0].actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, null));
+
+        // Verify that the cardLayout's show method was called with the expected arguments
+        Mockito.verify(money.cardLayout).show(money.contentPanel, "Savings");
+
+        // Verify that the content displayed is "Savings Content!"
+        assertEquals("Savings Content!", ((JLabel) ((JPanel) money.contentPanel.getComponent(2)).getComponent(0)).getText());
+    }
+}
